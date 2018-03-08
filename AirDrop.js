283a8675e3e66fb0cd6220469d6c394eb78cf726
ryzuo <joshuamurdoc@gmail.com>
2018-03-08 16:37:42 +0800

    Add transaction status check

--- AirDrop.js
+++ AirDrop.js
@@ -1,6 +1,11 @@
 var succTrans = new Array();
 var failTrans = new Array();
 
+var closedDeal = new Array();
+var redoDeal = new Array();
+
+var unknownDeal = new Array();
+
 function doTransfer(contOwner, ownerPwd) {
     //var nonce_ = 1001;
     //var macc = eth.accounts[0];
@@ -17,9 +22,9 @@ function doTransfer(contOwner, ownerPwd) {
             var bal = bill.bal;
             var idx = bill.idx;
             try {
-                var txHash = contInst.transfer(addr, web3.toWei(bal, 'ether'), {from: macc});
+                var txHash = contInst.transfer(addr, web3.toWei(bal, 'ether'), {from: macc}); //, gasPrice: 20000000000});
                 var trx = eth.getTransaction(txHash);
-                msg = "[" + idx + ":] " + txHash + ": {from: " + addr + ", val: " + bal + ", nonce: " + trx.nonce + "}";
+                msg = "[" + idx + ":] " + txHash + " {from: " + addr + ", val: " + bal + ", nonce: " + trx.nonce + "}";
                 succTrans.push(msg);
                 console.log(msg);
             } catch (e) {
@@ -34,4 +39,28 @@ function doTransfer(contOwner, ownerPwd) {
         }
     }
     this.execBatch = execBatch;
+}
+
+function showTransResults() {
+    succTrans.forEach(function (trans) {
+       var trxInfo = trans.split(" ");
+       var trxHash = trxInfo[1].split(":")[0];
+       var trxObj = eth.getTransaction(trxHash);
+       var trxBlockHash = trxObj.blockHash;
+
+       if (trxBlockHash.substr(2, 10) != "0000000000") {
+           var trxReceipt = eth.getTransactionReceipt(trxHash);
+           var trxStatus = trxReceipt.status;
+           if (trxStatus == "0x1") {
+               trxInfo += " | [Block Hash:] " + trxBlockHash;
+               closedDeal.push(trxInfo);
+               console.log(trxInfo);
+           }
+           else if (trxStatus == "0x0") {
+               redoDeal.push(trxInfo);
+           } else {
+               unknownDeal.push(trxInfo);
+           }
+       }
+    });
 }
